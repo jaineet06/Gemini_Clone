19b40fc8e26f2a47e5a46fbd09c6264e9a6056b6
jaineet06 <jaineetshah06@gmail.com>
2024-08-31 16:18:21 +0530

    Files Uploaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './Components/Sidebar/Sidebar.jsx'
+import Main from './Components/Main/Main.jsx';
+
+function App() {
+    return(
+      <>
+      <Sidebar/>
+      <Main/>
+      </>
+    );
+}
+
+export default App
